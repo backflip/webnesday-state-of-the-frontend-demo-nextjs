de2a5e215f43b0da7b376d79e1935600e41c9f51
Thomas Jaggi <thomas@responsive.ch>
2024-04-02 23:46:14 +0200

    feat: add modal

--- src/app/components/Pizze.tsx
+++ src/app/components/Pizze.tsx
@@ -1,11 +1,39 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Modal from "react-modal";
 import type { ApiPizzeResponse } from "../api/pizze/route";
 
 type Pizze = {
   [key: string]: string[];
 };
+
+function Pizza({
+  name,
+  toppings,
+}: {
+  name: keyof Pizze;
+  toppings: Pizze[string];
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <h2>{name}</h2>
+      <button onClick={() => setIsOpen(true)}>Details</button>
+      <Modal isOpen={isOpen} contentLabel={String(name)}>
+        <h1>{name}</h1>
+        <button onClick={() => setIsOpen(false)}>close</button>
+
+        <ul>
+          {toppings.map((topping) => (
+            <li key={topping}>{topping}</li>
+          ))}
+        </ul>
+      </Modal>
+    </>
+  );
+}
 
 export default function Pizze() {
   const [pizze, setPizze] = useState<Pizze>();
@@ -38,13 +66,7 @@ export default function Pizze() {
     <ul>
       {Object.entries(pizze).map(([name, toppings]) => (
         <li key={name}>
-          <h2>{name}</h2>
-        
-          <ul>
-            {toppings.map((topping) => (
-              <li key={topping}>{topping}</li>
-            ))}
-          </ul>
+          <Pizza name={name} toppings={toppings} />
         </li>
       ))}
     </ul>
